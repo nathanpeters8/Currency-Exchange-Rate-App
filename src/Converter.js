a8f9844965f87a30255db491a4ca97f65b3d2c2e
Nathan Peters <peters.nate8@gmail.com>
2023-11-22 16:31:10 -0700

    add submit listener for converter amount input

--- src/Converter.js
+++ src/Converter.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 class Converter extends React.Component {
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(document.getElementById('amount').value);
+  }
   render() {
     return (
       <>
         <div className='row justify-content-center mt-5'>
-          <div className='col-6 col-md-3 d-flex flex-column align-items-center mb-4 px-0'>
+          <div className='col-6 d-flex flex-column align-items-center mb-4 px-0'>
             <label className='form-label'>Amount</label>
-            <div className= 'input-group' id='amount-input'>
-              <span className='input-group-text'>$</span>
-              <input type='text' className='form-control ms-0' />
-            </div>
+            <form onSubmit={this.handleSubmit} className='form-inline'>
+              <div className= 'input-group' id='amount-input'>
+                <span className='input-group-text'>$</span>
+                <input type='text' className='form-control' id='amount'/>
+                <button type='submit' className='btn btn-success'>Submit</button>
+              </div>
+            </form>
           </div>
         </div>
       </>
