a12bbdf68062d70e537495a2cc55553c9b8afa32
Nathan Peters <peters.nate8@gmail.com>
2023-11-22 15:47:22 -0700

    add amount input for converter page

--- src/Converter.js
+++ src/Converter.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Converter extends React.Component {
   render() {
-    return <h1 className='text-center'>Currency Converter</h1>;
+    return (
+      <>
+        <div className='row justify-content-center mt-5'>
+          <div className='col-6 col-md-3 d-flex flex-column align-items-center mb-4 px-0'>
+            <label className='form-label'>Amount</label>
+            <div className= 'input-group' id='amount-input'>
+              <span className='input-group-text'>$</span>
+              <input type='text' className='form-control ms-0' />
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
--- src/Home.js
+++ src/Home.js
@@ -6,23 +6,21 @@ class Home extends React.Component {
     return (
       <>
         {/* Page Buttons */}
-        <div className="container-md">
-          <div className='row justify-content-center'>
-            <div className='col-8 d-flex'>
-              <Link to='/' className='btn btn-secondary flex-fill me-5'>
-                Currency Converter
-              </Link>
-              <Link to='/chart' className='btn btn-secondary flex-fill ms-5'>
-                Exchange Chart
-              </Link>
-            </div>
+        <div className='row justify-content-center'>
+          <div className='col-8 d-flex'>
+            <Link to='/' className='btn btn-secondary flex-fill me-5'>
+              Currency Converter
+            </Link>
+            <Link to='/chart' className='btn btn-secondary flex-fill ms-5'>
+              Exchange Chart
+            </Link>
           </div>
         </div>
         {/* Currency Dropdowns */}
-        <div className='container-md d-flex flex-column flex-md-row align-items-center align-items-md-start justify-content-md-around mt-4 px-md-5'>
+        <div className='row d-flex flex-column flex-md-row align-items-center align-items-md-start justify-content-md-around mt-4 px-md-5'>
           {/* From */}
-          <div className='col-6 col-md-3 d-flex flex-md-column align-items-center justify-content-center mb-4'>
-            <label className='form-label me-2'>From</label>
+          <div className='col-6 col-md-3 d-flex flex-column align-items-center justify-content-center mb-4'>
+            <label className='form-label'>From</label>
             <div className='dropdown-center col-12 d-flex'>
               <button
                 type='button'
@@ -53,10 +51,12 @@ class Home extends React.Component {
             </div>
           </div>
           {/* Switch Button */}
-          <button className='btn btn-warning align-self-center ms-4 ms-md-0'>Switch</button>
+          <div className="col-1 d-flex justify-content-center align-self-center">
+            <i className='btn btn-warning'>\/</i>
+          </div>
           {/* To */}
-          <div className='col-6 col-md-3 d-flex flex-md-column align-items-center justify-content-center mt-4 mt-md-0'>
-            <label className='form-label pe-4 me-1'>To</label>
+          <div className='col-6 col-md-3 d-flex flex-column align-items-center justify-content-center mt-4 mt-md-0'>
+            <label className='form-label'>To</label>
             <div className='dropdown-center col-12 d-flex'>
               <button
                 type='button'
